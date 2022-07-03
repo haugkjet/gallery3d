0d65231ec88c15035c1b0330bd41e6aa4c10d0eb
Kjetil Haugland <haugkjet@gmail.com>
2022-07-03 12:33:03 +0200

    Added PointerLock controls

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,7 +1,8 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { FirstPersonControls } from "three/examples/jsm/controls/FirstPersonControls";
+
+import { PointerLockControls } from "three/examples/jsm/controls//PointerLockControls.js";
 import Stats from "three/examples/jsm/libs/stats.module";
 import { EXRLoader } from "three/examples/jsm/loaders/EXRLoader.js";
 import { GUI } from "dat.gui";
@@ -20,13 +21,27 @@ const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   0.1,
-  1000
+  1500
 );
 camera.position.z = 16;
 camera.position.y = 5.0;
 
 var light = new THREE.AmbientLight(0x404040, 1.0);
 scene.add(light);
+
+let moveForward = false;
+let moveBackward = false;
+let moveLeft = false;
+let moveRight = false;
+let canJump = false;
+
+let prevTime = performance.now();
+const velocity = new THREE.Vector3();
+const direction = new THREE.Vector3();
+const vertex = new THREE.Vector3();
+const color = new THREE.Color();
+
+const objects: any = [];
 
 /*const light = new THREE.DirectionalLight(0xffffff, 1.0);
 light.position.set(12, 12, 7);
@@ -44,10 +59,10 @@ scene.add(light);*/
 
 const SKY_COLOR = 0x0e0353;
 const GROUND_COLOR = 0xd5f3ed;
-const SKY_SIZE = 100;
+const SKY_SIZE = 950;
 
 const vertexShader = `
-      varying vec3 vWorldPosition;
+ varying vec3 vWorldPosition;
             void main() {
                 vec4 worldPosition = modelMatrix * vec4( position, 1.0 );
                 vWorldPosition = worldPosition.xyz;
@@ -123,13 +138,90 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
-const controls = new FirstPersonControls(camera, renderer.domElement);
+const controls = new PointerLockControls(camera, renderer.domElement);
+let raycaster = new THREE.Raycaster(
+  new THREE.Vector3(),
+  new THREE.Vector3(0, -1, 0),
+  0,
+  10
+);
 
-controls.lookSpeed = 0.0525;
-controls.movementSpeed = 10;
-//controls.noFly = false;
-controls.lookVertical = false;
-//controls.lookAt(0, 3, 0);
+const blocker = document.getElementById("blocker");
+const instructions = document.getElementById("instructions");
+if (instructions && blocker) {
+  instructions.addEventListener("click", function () {
+    controls.lock();
+  });
+
+  controls.addEventListener("lock", function () {
+    instructions.style.display = "none";
+    blocker.style.display = "none";
+  });
+
+  controls.addEventListener("unlock", function () {
+    blocker.style.display = "block";
+    instructions.style.display = "";
+  });
+}
+
+scene.add(controls.getObject());
+const onKeyDown = function (event: any) {
+  switch (event.code) {
+    case "ArrowUp":
+    case "KeyW":
+      moveForward = true;
+      break;
+
+    case "ArrowLeft":
+    case "KeyA":
+      moveLeft = true;
+      break;
+
+    case "ArrowDown":
+    case "KeyS":
+      moveBackward = true;
+      break;
+
+    case "ArrowRight":
+    case "KeyD":
+      moveRight = true;
+      break;
+
+    case "Space":
+      camera.position.z = 16;
+      camera.position.y = 5.0;
+      camera.position.x = 0.0;
+      camera.lookAt(0, 0, 0);
+      break;
+  }
+};
+
+const onKeyUp = function (event: any) {
+  switch (event.code) {
+    case "ArrowUp":
+    case "KeyW":
+      moveForward = false;
+      break;
+
+    case "ArrowLeft":
+    case "KeyA":
+      moveLeft = false;
+      break;
+
+    case "ArrowDown":
+    case "KeyS":
+      moveBackward = false;
+      break;
+
+    case "ArrowRight":
+    case "KeyD":
+      moveRight = false;
+      break;
+  }
+};
+
+document.addEventListener("keydown", onKeyDown);
+document.addEventListener("keyup", onKeyUp);
 
 //controls.lookAt( scene.position );
 //renderer.toneMapping = THREE.ReinhardToneMapping;
@@ -161,6 +253,7 @@ loader.load(
         const m = child as THREE.Mesh;
         m.receiveShadow = true;
         m.castShadow = true;
+        objects.push(m);
       }
       if ((child as THREE.Light).isLight) {
         const l = child as THREE.Light;
@@ -192,7 +285,7 @@ function onWindowResize() {
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
   render();
-  controls.handleResize();
+  //controls.handleResize();
 }
 
 const stats = Stats();
@@ -218,15 +311,57 @@ cameraFolder.open();
 function animate() {
   requestAnimationFrame(animate);
 
+  const time = performance.now();
+
+  raycaster.ray.origin.copy(controls.getObject().position);
+  raycaster.ray.origin.y -= 10;
+
+  const intersections = raycaster.intersectObjects(objects, false);
+
+  const onObject = intersections.length > 0;
+
+  const delta = (time - prevTime) / 1000;
+
+  velocity.x -= velocity.x * 10.0 * delta;
+  velocity.z -= velocity.z * 10.0 * delta;
+
+  velocity.y -= 9.8 * 100.0 * delta; // 100.0 = mass
+
+  direction.z = Number(moveForward) - Number(moveBackward);
+  direction.x = Number(moveRight) - Number(moveLeft);
+  direction.normalize(); // this ensures consistent movements in all directions
+
+  if (moveForward || moveBackward) velocity.z -= direction.z * 400.0 * delta;
+  if (moveLeft || moveRight) velocity.x -= direction.x * 400.0 * delta;
+
+  if (onObject === true) {
+    velocity.y = Math.max(0, velocity.y);
+    canJump = true;
+  }
+
+  controls.moveRight(-velocity.x * delta);
+  controls.moveForward(-velocity.z * delta);
+
+  controls.getObject().position.y += velocity.y * delta; // new behavior
+
+  if (controls.getObject().position.y < 10) {
+    velocity.y = 0;
+    controls.getObject().position.y = 10;
+
+    canJump = true;
+  }
+
+  prevTime = time;
+
   render();
 
   stats.update();
 }
 
 function render() {
-  const delta = clock.getDelta();
+  //const delta = clock.getDelta();
 
-  controls.update(delta);
+  //controls.update(delta);
 
   renderer.render(scene, camera);
 }
